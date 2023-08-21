728c250b77c071fde20511a265f5673441c5641a
Lammyy5 <reliford2010@gmail.com>
2023-08-20 20:35:35 -0700

    added varibles to js

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+// elements 
+var highScoreEl = document.querySelector('#highscore')
+var timerEl = document.querySelector('#timer')
+var startBtn = document.querySelector('#start') 
+
+
+
+function startGame() {
+    // todo
+}
