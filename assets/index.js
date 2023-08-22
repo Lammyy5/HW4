cc86eb8daba5cbb9bb9facc537854d82150d7a67
lammyy5 <reliford2010@gmail.com>
2023-08-22 15:40:14 -0700

    added function to questions

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,12 @@
 // elements 
+var questionsEl = document.querySelector('.questions')
 var highScoreEl = document.querySelector('.highscore')
 var timerEl = document.querySelector('.timer')
 var startButton = document.querySelector('.start-button')
-let answers = ['alerts','parentheses','all of the above','quotes','console.log']
+let answersEl = ['alerts','parentheses','all of the above','quotes','console.log']
 let wrongAnswers = ['strings','booleans','numbers','square brackets','numbers and strings'
-,'other arrays','commas','curly brackets','periods','JavaScript','terminal','for loops'] 
+,'other arrays','commas','curly brackets','periods','JavaScript','terminal','for loops'];
+
 
 var timer =0;
 
@@ -16,16 +18,28 @@ function startQuiz() {
     console.log('starting quiz');
 
     startTimer()
-    nextQuestion()
+    Questions()
 
 }
 function startTimer() {
 console.log('Timer starting')
 highscore()
 }
-function nextQuestion() {
-   let answers = document.createElement('div')
-   
+function Questions() {
+   const firstQuestionEl = document.querySelector('.questions')
+   firstQuestion.innerHTML = `<h1>Commonly used data types DO NOT<br></br>
+   include:</h1>
+   <ul>
+   <li> <button>${wrongAnswers[0]}</button></li>
+   <li><button>${wrongAnswers[1]}</button></li>
+   <li><button>${answersEl[0]}</button></li>
+   <li><button>${wrongAnswers[2]}</button></li>
+   </ul>`
+
+questionsEl.appendChild(firstQuestionEl);
+}
+function answers(){
+answersEl.addEventListener('click', )
 }
 
 function highscore() {
