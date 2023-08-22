253e8c6cf0ed2dd2e32299c2c36c5c0b434c447f
lammyy5 <reliford2010@gmail.com>
2023-08-22 13:22:55 -0700

    added variables and functions

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,41 @@
 // elements 
-var highScoreEl = document.querySelector('#highscore')
-var timerEl = document.querySelector('#timer')
-var startBtn = document.querySelector('#start') 
+var highScoreEl = document.querySelector('.highscore')
+var timerEl = document.querySelector('.timer')
+var startButton = document.querySelector('.start-button')
+let answers = ['alerts','parentheses','all of the above','quotes','console.log']
+let wrongAnswers = ['strings','booleans','numbers','square brackets','numbers and strings'
+,'other arrays','commas','curly brackets','periods','JavaScript','terminal','for loops'] 
+
+var timer =0;
+
+// functions
 
 
+// Called when Start quiz button is pushed
+function startQuiz() {
+    console.log('starting quiz');
 
-function startGame() {
-    // todo
+    startTimer()
+    nextQuestion()
+
 }
+function startTimer() {
+console.log('Timer starting')
+highscore()
+}
+function nextQuestion() {
+   let answers = document.createElement('div')
+   
+}
+
+function highscore() {
+    console.log('highscore logged')
+}
+
+
+
+
+// Event listerners
+startButton.addEventListener("click", startQuiz);
+
+
