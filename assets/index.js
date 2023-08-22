25f635f4766f49c1c4109c8b69bd9f00e6430c81
Lammyy5 <reliford2010@gmail.com>
2023-08-22 16:43:06 -0700

    added to functions

--- assets/index.js
+++ assets/index.js
@@ -37,6 +37,54 @@ function Questions() {
    </ul>`
 
 questionsEl.appendChild(firstQuestionEl);
+
+const secondQuestionEl = document.querySelector('.questions')
+   secondQuestion.innerHTML = `<h1>Commonly used data types DO NOT<br></br>
+   include:</h1>
+   <ul>
+   <li> <button>${wrongAnswers[0]}</button></li>
+   <li><button>${wrongAnswers[1]}</button></li>
+   <li><button>${answersEl[0]}</button></li>
+   <li><button>${wrongAnswers[2]}</button></li>
+   </ul>`
+
+questionsEl.appendChild(secondQuestionEl);
+
+const thirdQuestionEl = document.querySelector('.questions')
+   thirdQuestion.innerHTML = `<h1>Commonly used data types DO NOT<br></br>
+   include:</h1>
+   <ul>
+   <li> <button>${wrongAnswers[0]}</button></li>
+   <li><button>${wrongAnswers[1]}</button></li>
+   <li><button>${answersEl[0]}</button></li>
+   <li><button>${wrongAnswers[2]}</button></li>
+   </ul>`
+
+questionsEl.appendChild(thirdQuestionEl);
+
+const fourthQuestionEl = document.querySelector('.questions')
+   fourthQuestion.innerHTML = `<h1>Commonly used data types DO NOT<br></br>
+   include:</h1>
+   <ul>
+   <li> <button>${wrongAnswers[0]}</button></li>
+   <li><button>${wrongAnswers[1]}</button></li>
+   <li><button>${answersEl[0]}</button></li>
+   <li><button>${wrongAnswers[2]}</button></li>
+   </ul>`
+
+questionsEl.appendChild(fourthQuestionEl);
+
+const fifthQuestionEl = document.querySelector('.questions')
+   fifthQuestion.innerHTML = `<h1>Commonly used data types DO NOT<br></br>
+   include:</h1>
+   <ul>
+   <li> <button>${wrongAnswers[0]}</button></li>
+   <li><button>${wrongAnswers[1]}</button></li>
+   <li><button>${answersEl[0]}</button></li>
+   <li><button>${wrongAnswers[2]}</button></li>
+   </ul>`
+
+questionsEl.appendChild(fifthQuestionEl);
 }
 function answers(){
 answersEl.addEventListener('click', )
@@ -51,5 +99,5 @@ function highscore() {
 
 // Event listerners
 startButton.addEventListener("click", startQuiz);
-
-
+answers.addEventListener('click', )
+wrongAnswers.addEventListener('click',)
