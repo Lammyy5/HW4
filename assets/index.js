81b5415c396ec4b1b47387017b3be477eac47f08
Lammyy5 <reliford2010@gmail.com>
2023-09-06 22:38:12 -0700

    added js

--- assets/index.js
+++ assets/index.js
@@ -9,72 +9,67 @@ let startButton = document.querySelector(".start-button");
 const questions = [
   {
     question: "Commonly used data types DO NOT include:",
-    correctAnswer: 'alerts',
     answers: [
-      'strings',
-      'booleans',
-      'alerts',
-      'numbers',
+      { Text: 'strings', correct: false },
+      { Text: 'booleans', correct: false },
+      { Text: 'alerts', correct: true },
+      { Text: 'numbers', correct: false },
     ]
-
   },
   {
     question: 'The condition in an if else statement is enclosed within:___',
-    correctAnswer: 'parentheses',
     answers: [
-      'parentheses',
-      'curly brackets',
-      'numbers and strings',
-      'square brackets',
+      { Text: 'parentheses', correct: true },
+      { Text: 'curly brackets', correct: false },
+      { Text: 'numbers and strings', correct: false },
+      { Text: 'square brackets', correct: false },
     ]
-
   },
   {
     question: 'Arrays in JavaScript can be used to store:____?',
-    correctAnswer: 'all of the above',
     answers: [
-      'strings',
-      'booleans',
-      'alerts',
-      'all of the above',
+      { Text: 'strings', correct: false },
+      { Text: 'booleans', correct: false },
+      { Text: 'alerts', correct: false },
+      { Text: 'all of the above', correct: true },
     ]
-
   },
   {
     question: 'String values must be enclosed within _____ when being assigned to variables<',
-    correctAnswer: 'quotes',
     answers: [
-      'curly brackets',
-      'quotes',
-      'commas',
-      'periods',
+      { Text: 'curly brackets', correct: false },
+      { Text: 'quotes', correct: true },
+      { Text: 'commas', correct: false },
+      { Text: 'periods', correct: false },
     ]
-
   },
   {
     question: 'A very useful during development and debugging for printing content to the debugger is:',
-    correctAnswer: 'console.log',
     answers: [
-      'console.log',
-      'terminal',
-      'for loops',
-      'JavaScript',
+      { Text: 'console.log', correct: true },
+      { Text: 'terminal', correct: false },
+      { Text: 'for loops', correct: false },
+      { Text: 'JavaScript', correct: false },
     ]
-
   }
-];
+]
 let nextQuestionindex = 0;
-let QI = 0
+let QI = 0;
+let score = 0;
 let sec = 5;
 // functions
 const answerButton = document.getElementById("answer-button");
 const timerEl = document.getElementById("timer");
+const nextButton = document.getElementById("next-btn");
 // Called when Start quiz button is pushed
 function startQuiz() {
   nextQuestionindex = 0;
+  score = 0;
+  // nextButton.innerHTML = "Next"
 
-  displayQuestions();
+  
   startTimer();
+  displayQuestions();
 
 
 };
@@ -82,7 +77,7 @@ function startTimer() {
   const timer = setInterval(function () {
     sec--;
     timerEl.innerHTML = "Time:" + sec;
-    if (sec <= 0) {
+    if (sec === 0) {
       clearInterval(timer);
       timerEl.innerHTML = "Time's up!"
     }
@@ -90,41 +85,81 @@ function startTimer() {
 }
 function displayQuestions() {
   resetState();
-  for (i = 0; i < questions.length;i++){
-    let quest = questions[QI].question;
-    var userAnswers = questions[QI].answers;
-    question.innerHTML = quest;
-  }
+  // head.innerHTML= "";
+  // question.innerHTML= "";
+  // answerButton.innerHTML ="";
+  // for (i = 0; i < questions.length;i++){
+  //   const quest = questions[QI].question;
+  //   var userAnswers = questions[QI].answers;
+  //   question.innerHTML = quest;
+  let nextQuestion = questions[QI];
+  let questionNumb = QI + 1;
+  question.innerHTML = questionNumb + ". " + nextQuestion.question;
 
-userAnswers.forEach(newBtn => {
+  nextQuestion.answers.forEach(answer => {
     const button = document.createElement("button");
-    button.innerHTML = newBtn;
+    button.innerHTML = answer.Text;
     button.classList.add("btn");
-    button.addEventListener('click', highscore())
-    
-    answerButton.appendChild(button)
-      })
-
-
-    
-    
+    answerButton.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct= answer.correct;
+    }
+    button.addEventListener('click',highscore);
+  })
   }
 
+// userAnswers.forEach(newBtn => {
+//     const button = document.createElement("button");
+//     button.textContent = newBtn;
+//     button.classList.add("btn");
+  
 
 
 
-function highscore() {
-  QI++;
-   
+
+function highscore(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+
+  Array.from(answerButton.children).forEach(button => {
+    if(button.dataset.correct = "true"){
+      score++;
+      console.log('true')
+    }
+    button.disabled = true;
+  });
+  // nextButton.style.display = "block"
+   displayQuestions();
   // todo
 };
+function buttonFunc(){
+  QI++;
+  if(QI < questions.length){
+    displayQuestions();
+  }
+}
+// try with displayQuestions function
+nextButton.addEventListener("click", ()=>{
+  if(QI < questions.length){
+     buttonFunc();
+  }
+})
 
 
+function endQuiz() {
+  question.innerHTML = "";
+  head.innerHTML= "";
+  answerButton.innerHTML = "";
 
+
+}
 function resetState() {
 
   startButton.style.display = 'none';
-  head.style.display = 'none';
+  while(answerButton.firstChild){
+    answerButton.removeChild(answerButton.firstChild);
+  }
+  
 };
 // Event listerners
 startButton.addEventListener("click", startQuiz);
